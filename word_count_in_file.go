65f77977ab4e298e5ccfacffd73e593ce549df54
Neel Thomas <neeltom92@gmail.com>
2019-06-29 01:13:08 +0530

    Create word_count_in_file.go

---
+++ word_count_in_file.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var j int
+
+func main() {
+  j=0
+
+	file, err := os.Open("PATH_TO_THE_FILE/wordcount-1.txt")
+	_ = err
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+
+		log_file := scanner.Text()
+    st1 := strings.Split(log_file, " ")
+    j=j+len(st1)
+
+
+	}
+
+fmt.Println(j)
+
+
+}
